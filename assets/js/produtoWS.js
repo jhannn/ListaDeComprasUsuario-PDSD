d89c0eaa9d39dce260d5113125a0211da5ef7ba6
Jonnathan Bruno <jonnathanbruno@gmail.com>
2015-01-31 12:58:17 -0300

    ListarTodosOsProdutos

--- assets/js/produtoWS.js
+++ assets/js/produtoWS.js
@@ -38,3 +38,59 @@ function autoComplete(){
         }
     });	
 }
+
+function listarProduto()
+{
+	var produto = ["Feijao","Arroz"];
+	
+	for(var i=0 ;i<produto.length ;i++)
+	{
+		listaEstilo(produto[i],"descricao do produto");
+	}
+}
+
+//---------- Construção de HTML no javascript --------------//
+function listaEstilo(nomeLista,descricao)
+{
+	var divPrincipal = document.createElement("div");
+		var divRole = document.createElement("div");
+		var h4 = document.createElement("h4");
+		var a = document.createElement("a");
+		var img = document.createElement("img");
+		var divP = document.createElement("div");
+		var painel = document.createElement("div");
+		var nomeProduto = document.createElement("p");
+		
+		//--estilos--
+		divPrincipal.setAttribute("class","panel panel-default");
+		divRole.setAttribute("class","panel-heading");
+		h4.setAttribute("class","panel-title");
+		a.setAttribute("style","color: #ffb503;");
+		
+		img.setAttribute("src","assets/img/detalhes.png");
+		img.setAttribute("width","30px");
+		img.setAttribute("style","color: #ffb503;");
+		
+		nomeProduto.setAttribute("class","ajustesLista");
+		nomeProduto.innerHTML = nomeLista;
+		
+		divP.setAttribute("id",i);
+		painel.setAttribute("class","panel-body");
+		painel.innerHTML = descricao;
+		
+		//--------//
+		
+		divPrincipal.appendChild(divRole);
+		divPrincipal.appendChild(h4);
+		divPrincipal.appendChild(a);
+		divPrincipal.appendChild(img);
+		divPrincipal.appendChild(divP);
+		divRole.appendChild(h4);
+		h4.appendChild(a);
+		h4.appendChild(nomeProduto);
+		a.appendChild(img);
+		divP.appendChild(painel);
+		
+		var pai = document.getElementById("referencia");
+		pai.appendChild(divPrincipal);	
+}
