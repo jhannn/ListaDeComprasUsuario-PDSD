b07240027e14ee09414a2044a97ac985b4db4cbe
Jonnathan Bruno <jonnathanbruno@gmail.com>
2015-01-31 15:49:30 -0300

    ListarProdutos-Completo

--- assets/js/produtoWS.js
+++ assets/js/produtoWS.js
@@ -41,16 +41,30 @@ function autoComplete(){
 
 function listarProduto()
 {
-	var produto = ["Feijao","Arroz"];
-	
-	for(var i=0 ;i<produto.length ;i++)
-	{
-		listaEstilo(produto[i],"descricao do produto");
-	}
+	$.ajax({
+        type: 'POST'
+        , url: "http://localhost:52192/Servidor/Produto.asmx/retornarTodosProdutos"
+		, crossDomain:true
+        , contentType: 'application/json; charset=utf-8'
+        , dataType: 'json'
+        , data: "{idUsuario:'"+ID_USUARIO+"',token:'"+TOKEN+"'}"
+		, success: function (data, status){  
+		
+			var produto = $.parseJSON(data.d); 
+			for(var i=0 ;i<produto.length ;i++)
+			{
+			listaEstilo(produto[i]);
+			}
+			
+        }
+        , error: function (xmlHttpRequest, status, err) {
+            $('.resultado').html('Ocorreu um erro');
+        }
+    });	
 }
 
 //---------- Construção de HTML no javascript --------------//
-function listaEstilo(nomeLista,descricao)
+function listaEstilo(produto)
 {
 	var divPrincipal = document.createElement("div");
 		var divRole = document.createElement("div");
@@ -71,12 +85,17 @@ function listaEstilo(nomeLista,descricao)
 		img.setAttribute("width","30px");
 		img.setAttribute("style","color: #ffb503;");
 		
-		nomeProduto.setAttribute("class","ajustesLista");
-		nomeProduto.innerHTML = nomeLista;
+		nomeProduto.setAttribute("class","ajustes-lista");		
+		nomeProduto.innerHTML = produto.nome;
 		
-		divP.setAttribute("id",i);
+		divP.setAttribute("id",produto.id_produto);
 		painel.setAttribute("class","panel-body");
-		painel.innerHTML = descricao;
+		painel.setAttribute("id","nome"+produto.id_produto);
+		painel.innerHTML =  "<p id='detalhes'> Marca: "+ produto.marca +
+							"<br /> Codigo de barras: "+ produto.codigoDeBarras +
+							"<br /> Tipo codigo de barras: "+ produto.tipoCodigoDeBarras +
+							"<br /> Unidade: "+ produto.unidade +
+							"<br /> Embalagem: "+ produto.embalagem +"</p> </ br>";
 		
 		//--------//
 		
@@ -93,4 +112,26 @@ function listaEstilo(nomeLista,descricao)
 		
 		var pai = document.getElementById("referencia");
 		pai.appendChild(divPrincipal);	
+		nomeProduto.setAttribute("onclick","abrirDescricao('"+ 'nome'+produto.id_produto+"')");
+}
+
+var aberta = "fechada";
+var controle = "-1";
+function abrirDescricao(id)
+{	
+
+	if(aberta == "fechada" && id != controle)//tem q abrir
+	{
+		document.getElementById(id).className = "panel-body-abrir";
+		controle = id;
+		aberta = "aberta";
+		return;
+	}
+	
+	if(aberta == "aberta" && id == controle) //tem q fechar
+	{
+		document.getElementById(id).className = "panel-body";
+		controle = "-1";
+		aberta = "fechada";
+	}
 }
