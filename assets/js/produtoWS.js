0af331666d24c4692d77c1dc03c172c318317aa4
Jonnathan Bruno <jonnathanbruno@gmail.com>
2015-02-02 20:24:22 -0300

    Ajustes em pesquisar produto

--- assets/js/produtoWS.js
+++ assets/js/produtoWS.js
@@ -1,24 +1,27 @@
 var ID_USUARIO = window.localStorage.UsuarioId;
 var TOKEN = window.localStorage.UsuarioToken;
 
-//__________________________________ CADASTRAR PRODUTO ______________________________________//
-var listaDeProdutos = [];
-var i = 0;
-function cadastrarProduto(){
-	var codigoDeBarras = $("#cod_barra").val();
-	var nomeDoProduto = $("#nome_produto").val();
-	var formatoCodigoDeBarras = $("#formato").val();
-	if(codigoDeBarras==""){
-		codigoDeBarras=-1;
-		formatoCodigoDeBarras=-1;
-	}	
-	var produtos = new Array(codigoDeBarras,formatoCodigoDeBarras,nomeDoProduto);
-	listaDeProdutos[i++] = produtos;	
-	window.location = "principal.html#editar_lista"
-	document.getElementById("exibir").innerHTML = "- " + listaDeProdutos[i-1][2] +"<br />";
+//______________________________ AUTO COMPLETE MARCA _______________________________________// 
+function autoCompleteMarca(){
+	
+	var nomeMarca = $("#marcaProduto").val();
+	$.ajax({
+        type: 'POST'
+        , url: "http://localhost:52192/Servidor/Produto.asmx/autocompleteMarca"
+		, crossDomain:true
+        , contentType: 'application/json; charset=utf-8'
+        , dataType: 'json'
+        , data: "{idUsuario:'"+ID_USUARIO+"',token:'"+TOKEN+"',nomeMarca:'"+nomeMarca+"'}"
+		, success: function (data, status){                    
+			var marcas = $.parseJSON(data.d); //salvando o nome das marcas em um array
+			$("#marcaProduto").autocomplete({ source: marcas }); 
+        }
+        , error: function (xmlHttpRequest, status, err) {
+            $('.resultado').html('Ocorreu um erro');
+        }
+    });	
 }
-
-//______________________________ AUTO COMPLETE _______________________________________// 
+//______________________________ AUTO COMPLETE PRODUTO _______________________________________// 
 function autoComplete(){
 	
 	var nomeProduto = $("#nomeDoProduto").val();
@@ -40,89 +43,45 @@ function autoComplete(){
 }
 
 //_______________________ PESQUISAR PRODUTO POR NOME ___________________________//
-function pesquisarProdutosNome()
+function pesquisarProduto()
 {
-	var marca = $("#marca_produto_pesquisa").val();
+
 	var nome = $("#nomeDoProduto").val();
-	
-	$.ajax({
-        type: 'POST'
-        , url: "http://localhost:52192/Servidor/Produto.asmx/pesquisarProdutosNome"
-		, crossDomain:true
-        , contentType: 'application/json; charset=utf-8'
-        , dataType: 'json'
-        , data: "{idUsuario:'"+ID_USUARIO+"',token:'"+TOKEN+"',marca:'"+marca+"',nome:'"+nome+"'}"
-		, success: function (data, status){  
-		
-			var produto = $.parseJSON(data.d);
-			if(produto.erro == "Erro de Pesquisa") 
-			{
-				alert(produto.Message);
-			}
-			else
-			{	
-				for(var i=0 ;i<produto.length ;i++)
-				{ listaEstilo(produto[i]); }	
-			}
-        }
-        , error: function (xmlHttpRequest, status, err) {
-            $('.resultado').html('Ocorreu um erro');
-        }
-    });	
-}
-
-//_______________________ PESQUISAR PRODUTO POR MARCA ___________________________//
-function pesquisarProdutosMarca()
-{
-	var marca = $("#marca_produto_pesquisa").val();
-	
-	$.ajax({
-        type: 'POST'
-        , url: "http://localhost:52192/Servidor/Produto.asmx/pesquisarProdutosMarca"
-		, crossDomain:true
-        , contentType: 'application/json; charset=utf-8'
-        , dataType: 'json'
-        , data: "{idUsuario:'"+ID_USUARIO+"',token:'"+TOKEN+"',marca:'"+marca+"'}"
-		, success: function (data, status){  
-		
-			var produto = $.parseJSON(data.d);
-			if(produto.erro == "Erro de Pesquisa") 
-			{
-				alert(produto.Message);
-			}
-			else
-			{	
-				for(var i=0 ;i<produto.length ;i++)
-				{ listaEstilo(produto[i]); }	
-			}
-        }
-        , error: function (xmlHttpRequest, status, err) {
-            $('.resultado').html('Ocorreu um erro');
-        }
-    });	
-}
-
-//_______________________ PESQUISAR PRODUTO POR EMBALAGEM ___________________________//
-function pesquisarProdutosEmbalagem()
-{
 	var marca = $("#marcaProduto").val();
-	var nome = $("#nomeProduto").val();
-	var embalagem = $("#embalagem_produto_pesquisa").val();
+	var embalagem = $('select[name=embalagem]').val(); 
+	
+	var dados;
+	var url;
+	
+	//------ Pesquisar por nome -----//
+	if(nome!="" && (marca!="" || marca=="" ) && embalagem==""){
+		dados = "{idUsuario:'"+ID_USUARIO+"',token:'"+TOKEN+"',marca:'"+marca+"',nome:'"+nome+"'}"
+		url   = "http://localhost:52192/Servidor/Produto.asmx/pesquisarProdutosNome";
+	}
+	
+	//------ Pesquisar por marca -----//
+	if(nome == "" && marca != "" && embalagem == ""){
+		dados = "{idUsuario:'"+ID_USUARIO+"',token:'"+TOKEN+"',marca:'"+marca+"'}"
+		url = "http://localhost:52192/Servidor/Produto.asmx/pesquisarProdutosMarca"	
+	}
+
+	//------ Pesquisar por embalagem ----//
+	if(nome != "" && marca != "" && embalagem != ""){
+		dados =  "{idUsuario:'"+ID_USUARIO+"',token:'"+TOKEN+"',marca:'"+marca+"',nome:'"+nome+"',embalagem:'"+embalagem+"'}"
+		url = "http://localhost:52192/Servidor/Produto.asmx/pesquisarProdutosEmbalagem"
+	}
 	
 	$.ajax({
         type: 'POST'
-        , url: "http://localhost:52192/Servidor/Produto.asmx/pesquisarProdutosEmbalagem"
+        , url: url
 		, crossDomain:true
         , contentType: 'application/json; charset=utf-8'
         , dataType: 'json'
-        , data: "{idUsuario:'"+ID_USUARIO+"',token:'"+TOKEN+"',marca:'"+marca+"',nome:'"+nome+"',embalagem:'"+embalagem+"'}"
+        , data: dados
 		, success: function (data, status){  
-		
 			var produto = $.parseJSON(data.d);
-			if(produto.erro == "Erro de Pesquisa") 
-			{
-				alert(produto.Message);
-			}
+			
+			if(produto.erro == "Erro de Pesquisa") {	alert(produto.Message);	}
 			else
 			{	
 				for(var i=0 ;i<produto.length ;i++)
